b6ea2daaeddc600f6650924d8f4f6bb76bebaf64
Oleksii-coder4 <loklom.pp@gmail.com>
2023-07-06 11:55:06 +0300

    addMemoToOldMAtador

--- src/components/OldMatador.tsx
+++ src/components/OldMatador.tsx
@@ -2,33 +2,6 @@ import { memo, useMemo, useState } from "react";
 import React from "react";
 import img from './imgsAndAudio/matador.jpg'
 import audio from './imgsAndAudio/mixkit-rhythmic-audience-clapping-loop-522.wav'
-// export const Matador = memo( (
-//     {applause, setMatarodPosition, matadorPosition}: 
-//     {applause: number, setMatarodPosition: (param: number) => void, matadorPosition: number}
-//     ) => {
-//     const [bullPosition, setBullPosition] = useState(null)
-
-//     // меняю позицию матадора только когда  изменяется позиция быка 
-//     useMemo(() => {
-//         if(bullPosition === matadorPosition) {
-//             checkMatadorPosition()
-//         }
-//     }, [bullPosition])
-//     let renderAudio: any 
-//     let a: any
-//     useMemo(() => {
-//         if(applause === 3) {
-//             console.log('only one time')
-//             renderAudio = < audio style={{visibility: "hidden"}}controls autoPlay> <source src={audio} type="audio/mpeg" /> </audio>
-//             a = <h1>Я отрондерился)</h1> 
-//         }
-//     }, [applause])
-
-// },
-//     (prevProps, nextProps) => {
-//         return prevProps.applause === nextProps.applause 
-//       }  
-//  )
 
 interface MyComponentState {
     bullPosition: number | null;
@@ -70,6 +43,12 @@ class OldMatador extends React.Component<MyComponentProps, MyComponentState> {
     } else {
       this.checkMatadorPosition();
     }
+  }
+  shouldComponentUpdate(nextProps: MyComponentProps) {
+    if (nextProps.applause !== this.props.applause) {
+      return true; // Обновляем компонент
+    }
+    return false; // Не обновляем компонент
   }
 
   render() {
