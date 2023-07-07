104a6bf535170f9734956c48fd42038e288cb8b1
Oleksii-coder4 <loklom.pp@gmail.com>
2023-07-07 19:33:42 +0300

    commit(

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <ArenaWithBull
-        matador={<OldMatador />} 
+        matador={<Matador />} 
       />
     </div>
   )
--- src/components/Matador.tsx
+++ src/components/Matador.tsx
@@ -1,29 +1,35 @@
-import { memo, useEffect, useMemo, useState } from "react";
+import { memo, useEffect, useMemo, useRef, useState } from "react";
 import img from './imgsAndAudio/youngMatador.gif'
 import audio from './imgsAndAudio/mixkit-rhythmic-audience-clapping-loop-522.wav'
 export const Matador = memo( ({applause, setMatarodPosition, matadorPosition}: {applause: number, setMatarodPosition: (param: number) => void, matadorPosition: number}) => {
-    const [bullPosition, setBullPosition] = useState(null)
-    document.addEventListener('bullRun', function getBullPosition(event: Event) {
-        const customEvent = event as CustomEvent
-        setBullPosition(customEvent.detail.position)
-        document.removeEventListener('bullRun', getBullPosition)
-    });
+    const [statePos, setBullPosition] = useState(null)
+    let bullPositionNoState: any = useRef(null)
+    useEffect(() => {
+        const getBullPosition = (event: Event) => {
+            const customEvent = event as CustomEvent
+            bullPositionNoState.current = customEvent.detail.position
+        };
+        document.addEventListener('bullRun', getBullPosition);
+        return () => {
+          document.removeEventListener('bullRun', getBullPosition);
+        };
+      }, []);
     function checkMatadorPosition() {
-        let futureMatadorPosition =  Math.floor(Math.random() * 8) 
-        if(futureMatadorPosition !== bullPosition) {
-            console.log(`Matador is moving from ${matadorPosition} to ${futureMatadorPosition}`)
-            setMatarodPosition(futureMatadorPosition)
-            return null
-        }else{
-            return checkMatadorPosition()
+        let futureMatadorPosition = 0
+        while(futureMatadorPosition === bullPositionNoState.current) {
+            futureMatadorPosition =  Math.floor(Math.random() * 8) 
         }
+        console.log(`bullPosition - ${statePos}, bullPosition witout state - ${bullPositionNoState.current}`)
+        console.log(`Matador is moving from ${matadorPosition} to ${futureMatadorPosition}`)
+        setMatarodPosition(futureMatadorPosition)
     }
     // меняю позицию матадора только когда  изменяется позиция быка 
     useEffect(() => {
-        if(bullPosition === matadorPosition) {
+        console.log('use Effect' + bullPositionNoState.current)
+        if(bullPositionNoState.current === matadorPosition) {
             checkMatadorPosition()
         }
-    }, [bullPosition])
+    }, [bullPositionNoState.current, matadorPosition])
     let renderAudio: any 
     useEffect(() => {
         if(applause === 3) {
